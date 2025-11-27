c47c2096c3ec91f88a5cb16262c80a9287739bc9
DrxCode-tech <anietiedaniel11@gmail.com>
2025-11-27 11:31:41 +0100

    tycy

--- hub-react/vite.config.js
+++ hub-react/vite.config.js
@@ -3,10 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",   // REQUIRED FOR VERCEL
+  base: "/",
+  build: {
+    outDir: "dist"   // <-- THIS CREATES hub-react/dist
+  },
   server: {
     proxy: {
       "/api": "http://localhost:3000"
     }
-  },
+  }
 });
